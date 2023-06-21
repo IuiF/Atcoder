349ed96045754291e12e72439b62a570a6c87547
IuiF <118101992+IuiF@users.noreply.github.com>
2023-06-21 18:04:47 +0000

    main

--- contest/abc137/b/main.py
+++ contest/abc137/b/main.py
@@ -1,4 +1,4 @@
 k, x = map(int, input().split())
-nums = list(range(-150, 150))
+nums = list(range(-15000000, 15000000))
 
-print(*nums[(x - k + 151) : (x + k + 150)])
+print(*nums[(x - k + 15000001) : (x + k + 15000000)])
