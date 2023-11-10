dc3c21a7f94d5b2047530a5f2f7e7b45da68f621
IuiF <118101992+IuiF@users.noreply.github.com>
2023-11-10 12:05:09 +0900

    main

--- contest/abc205/c/main.py
+++ contest/abc205/c/main.py
@@ -1,1 +1,9 @@
+def solve(a, b, c):
+    if c % 2 == 0 or (a > 0 and b > 0):
+        return "<" if abs(a) < abs(b) else ">" if abs(a) > abs(b) else "="
+    else:
+        return "<" if a < b else ">" if a > b else "="
+
+
 a, b, c = map(int, input().split())
+print(solve(a, b, c))
