903ea2cf2b2ad7f564aba2d4a7d6f88c32e51068
IuiF <i.fukazawa.602@gmail.com>
2024-02-19 16:39:38 +0900

    main

--- contest/abc198/c/main.py
+++ contest/abc198/c/main.py
@@ -1,1 +1,10 @@
-r, x, y = map(int, input().split())
+from decimal import Decimal
+
+r, x, y = map(Decimal, input().split())
+L = (x**2 + y**2) ** Decimal("0.5")
+if L < r:
+    print(2)
+elif L % r == 0:
+    print(L // r)
+else:
+    print(L // r + 1)
